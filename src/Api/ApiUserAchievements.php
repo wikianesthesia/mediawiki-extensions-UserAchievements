06ae922e67a6ecc5bdd0eea36bce7d59f68b3a07
Chris Rishel <rishel@gmail.com>
2021-12-10 18:40:59 -0800

    Fixed bug with module group constant

--- src/Api/ApiUserAchievements.php
+++ src/Api/ApiUserAchievements.php
@@ -9,6 +9,8 @@ use MediaWiki\MediaWikiServices;
 
 class ApiUserAchievements extends ApiBase {
 
+    private const MODULE_GROUP = 'uaaction';
+
     /**
      * @var ApiModuleManager
      */
@@ -29,7 +31,7 @@ class ApiUserAchievements extends ApiBase {
             MediaWikiServices::getInstance()->getObjectFactory()
         );
 
-        $this->moduleManager->addModules( self::$uaActions, 'uaaction' );
+        $this->moduleManager->addModules( self::$uaActions, self::MODULE_GROUP );
     }
 
     public function getModuleManager() {
@@ -42,12 +44,12 @@ class ApiUserAchievements extends ApiBase {
         $params = $this->extractRequestParams();
 
         /** @var $module ApiUserAchievementsBase */
-        $module = $this->moduleManager->getModule( $params[ 'uaaction' ], 'uaaction' );
+        $module = $this->moduleManager->getModule( $params[ self::MODULE_GROUP ], self::MODULE_GROUP );
 
         // The checks for POST and tokens are the same as ApiMain.php
         $wasPosted = $this->getRequest()->wasPosted();
         if ( !$wasPosted && $module->mustBePosted() ) {
-            $this->dieWithErrorOrDebug( [ 'apierror-mustbeposted', $params[ 'pgaction' ] ] );
+            $this->dieWithErrorOrDebug( [ 'apierror-mustbeposted', $params[ self::MODULE_GROUP ] ] );
         }
 
         if ( $module->needsToken() ) {
@@ -71,7 +73,7 @@ class ApiUserAchievements extends ApiBase {
      */
     public function getAllowedParams() {
         return [
-            'uaaction' => [
+            self::MODULE_GROUP => [
                 ApiBase::PARAM_REQUIRED => true,
                 ApiBase::PARAM_TYPE => 'submodule',
             ],
@@ -81,9 +83,9 @@ class ApiUserAchievements extends ApiBase {
 
     public function isWriteMode() {
         // We can't use extractRequestParams() here because getHelpFlags() calls this function,
-        // and we'd error out because the uaaction parameter isn't set.
-        $moduleName = $this->getMain()->getVal( 'uaaction' );
-        $module = $this->moduleManager->getModule( $moduleName, 'uaaction' );
+        // and we'd error out because the group parameter isn't set.
+        $moduleName = $this->getMain()->getVal( self::MODULE_GROUP );
+        $module = $this->moduleManager->getModule( $moduleName, self::MODULE_GROUP );
         return $module ? $module->isWriteMode() : false;
     }
 
